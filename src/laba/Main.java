0823ccc7212a62ee9e319dc0047dbe073a4ea143
web.witcher <skaliushartem@gmail.com>
2023-11-08 15:51:25 +0300

    Update Main.java

--- src/laba/Main.java
+++ src/laba/Main.java
@@ -1,19 +1,26 @@
 package laba;
 
-import laba.pokemons.Relicanth;
-import laba.pokemons.Victreebel;
+import laba.pokemons.*;
 import ru.ifmo.se.pokemon.Battle;
 import ru.ifmo.se.pokemon.Pokemon;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello pokemons!");
+        System.out.println("Да начнётся битва!");
 
         Battle battle = new Battle();
-        Pokemon p1 = new Relicanth("Чужой", 1);
-        Pokemon p2 = new Victreebel("Хищник", 1);
+        Pokemon p1 = new Bellsprout("Чужой", 1);
+        Pokemon p2 = new MimeJr("Хищник", 1);
+        Pokemon p3 = new MrMime("Монстр", 1);
+        Pokemon p4 = new Relicanth("Убийца", 1);
+        Pokemon p5 = new Victreebel("Разрушитель", 1);
+        Pokemon p6 = new Weepinbell("Каратель", 1);
         battle.addAlly(p1);
+        battle.addAlly(p3);
+        battle.addAlly(p5);
         battle.addFoe(p2);
+        battle.addFoe(p4);
+        battle.addFoe(p6);
         battle.go();
     }
 }
